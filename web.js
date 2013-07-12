71164c1b2e8a5ef315fa0d52e5d3a2ae55fafb67
Thomas Trenner <thomas.trenner@gmail.com>
2013-07-12 22:23:56 +0100

    Setting up database

---
+++ web.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express.createServer(express.logger())
+var pg = require('pg'),
+  connectionString = process.env.DATABASE_URL,
+  start = new Date(),
+  port = process.env.PORT || 3000,
+  client,
+  query;
+
+client = new pg.Client(connectionString);
+client.connect();
+query = client.query('CREATE TABLE visits (date date)');
+query.on('end',function() {client.end();});
+
+app.get('/', function(request, response) {
+	var date = new Date();
+	client.query('INSERT INTO visits(date) VALUES($1)'. [date]);
+
+  query = client.query('SELECT COUNT(date) AS count FROM visits WHERE date = $1', [date]);
+  query.on('row', function(result) {
+		console.log(result);
+    if (!result) {
+			return res.send('No data found');
+		} else {
+				res.send('Visits today: ' + result.count);
+		}
+	});
+});
+
+app.listen(port, function() {
+		console.log('Listening on:', port);
+});
